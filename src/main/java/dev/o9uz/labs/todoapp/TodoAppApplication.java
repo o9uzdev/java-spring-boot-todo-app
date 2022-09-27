548ab329ae46e8e55a3fa9235740ae77430d662e
o9uzdev <me@o9uz.dev>
2022-09-27 21:25:26 +0300

    Edited pom.xml and added entity

--- src/main/java/dev/o9uz/labs/todoapp/TodoAppApplication.java
+++ src/main/java/dev/o9uz/labs/todoapp/TodoAppApplication.java
@@ -1,13 +1,30 @@
 package dev.o9uz.labs.todoapp;
 
+import dev.o9uz.labs.todoapp.entity.Todo;
+import dev.o9uz.labs.todoapp.entity.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class TodoAppApplication {
+public class TodoAppApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoAppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		User user = new User();
+		user.setId(1L);
+		user.setPassword("hashed Password");
+		user.setUsername("o9uz");
+
+		Todo todo = new Todo();
+		todo.setId(1L);
+		todo.setContent("Go to cinema");
+
+		user.getTodoList().add(todo);
+	}
 }
